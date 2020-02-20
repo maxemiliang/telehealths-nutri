b87278d0a4ed9e66f3ad226f545d0a113293e1b8
Maxemilian Grönblom <maxemiliang@gmail.com>
2020-02-21 01:35:37 +0200

    Somewhere

--- src/components/userdashboard.tsx
+++ src/components/userdashboard.tsx
@@ -1,13 +1,31 @@
-import { Component, h } from "preact"
+import { Component, h, Ref, createRef } from "preact"
 import OpenFoodAPI, { defaultOptions } from "./../off"
 import Header from "./DashboardComponents/header"
 import Button from "./../elements/button"
-
-export default class UserDashboard extends Component {
-    public currentProduct = null
+import { BrowserBarcodeReader } from "@zxing/library"
+export default class UserDashboard extends Component<{ video: Ref<HTMLVideoElement> }> {
+    public video = createRef()
 
     public handleScan(e): void {
-        console.log(e.target.files)
+        const files: FileList = e.target.files
+        if (files.length > 0) {
+            const reader = new FileReader()
+            const file: File = e.target.files[0]
+            const codeReader = new BrowserBarcodeReader()
+
+            reader.onload = async (): Promise<void> => {
+                const fileData: string | ArrayBuffer = reader.result
+                try {
+                    await codeReader.decodeFromInputVideoDevice(undefined, this.video.current).then(result => {
+                        console.log(result)
+                    })
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+
+            reader.readAsDataURL(file)
+        }
     }
 
     public render(): JSX.Element {
@@ -15,7 +33,8 @@ export default class UserDashboard extends Component {
             <div class="w-screen h-screen gradient-2 flex justify-center items-center">
                 <div class="bg-white rounded-md p-10" style="height: 97vh; width: 97vw">
                     <Header />
-                    <input value={this.currentProduct} onInput={this.handleScan} type="file" accept="image/*" capture />
+                    <video ref={this.video} width="300" height="200" style="border: 1px solid gray"></video>
+                    <input onInput={this.handleScan} type="file" accept="image/*" capture />
                 </div>
             </div>
         )
