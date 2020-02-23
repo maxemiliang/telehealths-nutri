dca2fff6969095fb2183a06e4ad5f143463f95b4
Maxemilian Grönblom <maxemiliang@gmail.com>
2020-02-23 15:11:41 +0200

    Fixed building issue related to video type

--- src/components/userdashboard.tsx
+++ src/components/userdashboard.tsx
@@ -33,7 +33,7 @@ export default class UserDashboard extends Component<{ video: Ref<HTMLVideoEleme
             <div class="w-screen h-screen gradient-2 flex justify-center items-center">
                 <div class="bg-white rounded-md p-10" style="height: 97vh; width: 97vw">
                     <Header />
-                    <video ref={this.video} width="300" height="200" style="border: 1px solid gray"></video>
+                    <video ref={this.video} path="" width="300" height="200" style="border: 1px solid gray"></video>
                     <input onInput={this.handleScan} type="file" accept="image/*" capture />
                 </div>
             </div>
