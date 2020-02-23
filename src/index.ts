7a915656c5c81f01f1f6066918722b6904dd029a
Maxemilian Grönblom <maxemiliang@gmail.com>
2020-02-23 15:18:26 +0200

    Added sentry

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import App from "./components/app"
 import "./styles/tailwind.css"
+import * as Sentry from "@sentry/browser"
+
+Sentry.init({ dsn: "https://e97668d35d1c4c648f10c0cace4cce44@sentry.maxemiliang.cloud/5" })
 
 export default App
