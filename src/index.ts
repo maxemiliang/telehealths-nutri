3f37aa98bc5d5f3db8bedfbf3dcbaff5069b8a22
Maxemilian Grönblom <maxemiliang@gmail.com>
2020-02-24 00:24:09 +0200

    Better user feedback from Sentry

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import App from "./components/app"
 import "./styles/tailwind.css"
 import * as Sentry from "@sentry/browser"
 
-Sentry.init({ dsn: "https://e97668d35d1c4c648f10c0cace4cce44@sentry.maxemiliang.cloud/5" })
+Sentry.init({
+    dsn: "https://e97668d35d1c4c648f10c0cace4cce44@sentry.maxemiliang.cloud/5",
+    beforeSend(event, hint) {
+        // Check if it is an exception, and if so, show the report dialog
+        if (event.exception) {
+            Sentry.showReportDialog({ eventId: event.event_id })
+        }
+        return event
+    }
+})
 
 export default App
